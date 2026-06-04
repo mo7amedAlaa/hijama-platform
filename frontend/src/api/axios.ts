1cd4e25b29d3d63383bd5ff16190b555ebc13eab
mohamed3la3 <96891815+mo7amedAlaa@users.noreply.github.com>
2026-06-04 06:50:06 +0300

    cx

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosError } from "axios";
 
 // 👇 خذ الرابط من متغير بيئة
-const baseURL = import.meta.env.VITE_API_URL || "https://higamaplatform.com/api";
+const baseURL = import.meta.env.VITE_API_URL || "https://higamaplatform.com/api/api";
 
 const api = axios.create({
   baseURL,
